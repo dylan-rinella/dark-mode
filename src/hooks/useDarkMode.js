3e1e1358220892e25c2a218b998e0dc76f1c6fb1
dylan-rinella <rinelladylan@aim.com>
2021-01-13 17:25:41 -0500

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import useLocalStorage from './useLocalStorage'
+// // import darkMode from '../index.js';
+
+
+// const useDarkMode = (initialValues) => {
+//   const [values, setValues] = useLocalStorage("darkMode", initialValues)
+
+//   if(values.darkMode === true){
+//     setValues({
+//       darkMode: true
+//     })
+//   }
+
+// return ([values])
+
+// }
+
+// export default useDarkMode;
